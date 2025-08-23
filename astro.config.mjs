0a9d1770bc23a4b3ef4c0a37344397121af30a68
Taiyo <mechkimtaiyo@gmail.com>
2025-08-23 02:44:55 +0000

    configured github actions for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://taiyo14.github.io',
+  base: '/trb'
 });
